51ae62d409c050c266226b9ef4fe08c93f1dcc24
vitor ozorio <vitormanoelbasilio357@gmail.com>
2024-07-01 05:38:37 -0300

    configuring JPA

--- src/main/java/br/com/JPA_NelioAlves/Application/Programa.java
+++ src/main/java/br/com/JPA_NelioAlves/Application/Programa.java
@@ -2,12 +2,27 @@ package br.com.JPA_NelioAlves.Application;
 
 import br.com.JPA_NelioAlves.Dominio.Pessoa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Programa {
     public static void main(String[] args) {
 
-        Pessoa p1 = new Pessoa(1,"Vitor","suporte2@pontosocorro.com.br");
-        Pessoa p2 = new Pessoa(2,"Eduardo","suporte@pontosocorro.com.br");
-        Pessoa p3 = new Pessoa(3,"Renan","suporte3@pontosocorro.com.br");
+        Pessoa p1 = new Pessoa(null,"Vitor","suporte2@pontosocorro.com.br");
+        Pessoa p2 = new Pessoa(null,"Eduardo","suporte@pontosocorro.com.br");
+        Pessoa p3 = new Pessoa(null,"Renan","suporte3@pontosocorro.com.br");
+
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+        em.persist(p1);
+        em.persist(p2);
+        em.persist(p3);
+        em.getTransaction().commit();
+        System.out.println("Pronto");
 
         System.out.println(p1);
         System.out.println(p2);
--- src/main/java/br/com/JPA_NelioAlves/Dominio/Pessoa.java
+++ src/main/java/br/com/JPA_NelioAlves/Dominio/Pessoa.java
@@ -1,10 +1,16 @@
 package br.com.JPA_NelioAlves.Dominio;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
-
+@Entity
 public class Pessoa implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String email;
